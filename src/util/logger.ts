1e5e2378f66cf5040c09a87badb9c1ff5dde835f
İbrahim Duran <ibrahimeymenduran@gmail.com>
2017-07-06 02:40:21 +0300

    Added @types/colors package and fixed running out of colors
    This fixes the apparent issue of running out of colors

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,25 +1,21 @@
 import 'colors';
 
-export class Logger {
-  private static readonly colors = [
-    'red',
-    'green',
-    'yellow',
-    'blue',
-    'magenta',
-    'cyan',
-    'gray'
-  ];
+type LogColor = 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'gray';
 
-  private color: string;
+export class Logger {
+  private static colors: LogColor[] = Logger.getAvailableColors();
+
+  private color: LogColor;
   private label: string;
   private nsp?: Logger;
 
   private get prefix(): string {
-    return `${this.nsp ? this.nsp.prefix : `[${new Date().getTime()}]`} [${this.label[this.color as any]}]`;
+    return `${this.nsp ? this.nsp.prefix : `[${new Date().getTime()}]`} [${this.label[this.color]}]`;
   }
 
   constructor(label: string, nsp?: Logger) {
+    if (Logger.colors.length === 0) Logger.colors = Logger.getAvailableColors();
+
     this.label = label;
     this.color = Logger.colors.splice(Math.floor(Math.random() * Logger.colors.length), 1)[0];
     this.nsp = nsp;
@@ -33,5 +29,17 @@ export class Logger {
     console.log(`${this.prefix} ${msg}`);
 
     return this;
+  }
+
+  private static getAvailableColors() {
+    return [
+      'red',
+      'green',
+      'yellow',
+      'blue',
+      'magenta',
+      'cyan',
+      'gray'
+    ] as LogColor[];
   }
 }
