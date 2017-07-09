cef8543dd9cdac322525235959cc790421f05640
İbrahim Duran <ibrahimeymenduran@gmail.com>
2017-07-09 15:18:06 +0300

    Create specs for socket and network service

---
+++ test/network/network.spec.ts
@@ -0,0 +1,34 @@
+import 'mocha';
+import { assert } from 'chai';
+
+import { Kaptan, Network } from '../../build';
+
+describe('Network/Network', function () {
+  const kaptan = new Kaptan();
+  kaptan.use(Network.Network);
+
+  let network: Network.Network;
+  let socket: Network.Socket;
+
+  it('should spawn network service', function () {
+    network = kaptan.services.spawn('Network') as Network.Network;
+  });
+
+  it('should connect to another network', function (done) {
+    socket = network.connect('127.0.0.1', Network.Network.PORT as number);
+    socket.client.on('connect', () => done());
+  });
+
+  it('should transfer data between server and client', function (done) {
+    network.once('packet', (socket: Network.Socket, pkt: Network.Packet) => {
+      socket.send(pkt);
+    });
+
+    const packet = Network.Packet.raw('Hello, can you mirror this packet to me?');
+    socket.send(packet);
+    socket.on('packet', (pkt: Network.Packet) => {
+      assert.equal(pkt.toString(), packet.toString());
+      done();
+    });
+  });
+});
---
+++ test/network/socket.spec.ts
@@ -0,0 +1,73 @@
+import 'mocha';
+import { assert } from 'chai';
+
+import {
+  createServer,
+  createConnection,
+  Socket as NetSocket,
+  Server as NetServer
+} from 'net';
+import { Socket, Address, Packet, PacketProtocol } from '../../build/network';
+
+describe('Network/Socket', function () {
+  const PORT = 56743;
+
+  let server: NetServer;
+  let client: NetSocket;
+  let socket: Socket;
+
+  before(function (done) {
+    server = createServer().listen(PORT).on('listening', () => done());
+    server.on('connection', (socket: NetSocket) => {
+      socket.on('data', (chunk) => {
+        let str = chunk.toString();
+        let line = '';
+
+        for (let i = 0, len = str.length; i < len; i++) {
+          let chr = str[i];
+          line += chr;
+
+          if (/[\n\r]$/.test(chr)) {
+            socket.write(line);
+            line = '';
+          }
+        }
+      });
+    });
+
+    client = createConnection({ port: PORT });
+  });
+
+  it('should generate socket from netsocket', function () {
+    socket = Socket.fromNetSocket(client);
+  });
+
+  it('should send and receive packet', function (done) {
+    const packet = Packet.raw('hello from socket');
+    socket.send(packet);
+    socket.once('packet', (p) => {
+      assert.equal(p.toString(), packet.toString());
+      done();
+    });
+  });
+
+  it('should wait for specific packet', function (done) {
+    socket.wait({ data: 'wait_for_this' }).then(() => {
+      done();
+    });
+
+    socket.send('dont_mind_me');
+    socket.send('wait_for_this');
+  });
+
+  it('should send request and wait for response', function () {
+    socket.send({ protocol: PacketProtocol.REQUEST, data: 'hello rpc' })
+      .then((res: Packet) => {
+        assert.equal(res.data, 'hello rpc');
+      });
+  });
+
+  it ('should connect to server using address', function (done) {
+    new Socket(new Address('127.0.0.1', PORT)).on('connection', () => done());
+  });
+});
