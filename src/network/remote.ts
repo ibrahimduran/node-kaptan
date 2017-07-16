67000ce82c19d20ecb2f20686d1330c146bfeb2d
İbrahim Duran <ibrahimeymenduran@gmail.com>
2017-07-17 02:31:59 +0300

    Emit init event on initialization success

--- src/network/remote.ts
+++ src/network/remote.ts
@@ -75,6 +75,7 @@ export class RemoteService extends Service {
       if (packet.data && packet.data.error) {
         throw new Error((packet.data as any).error);
       } else {
+        this.emit('init');
         this.clientLogger.text('connection established');
       }
     });
