89ab0e1d51c6a3ebc5306551cd6e48e32cb6754c
İbrahim Duran <ibrahimeymenduran@gmail.com>
2017-08-05 12:23:52 +0300

    Add tests & refactor existings for changes in remote services

--- test/network/remote.spec.ts
+++ test/network/remote.spec.ts
@@ -3,75 +3,89 @@ import 'mocha';
 import * as getPort from 'get-port';
 import { assert } from 'chai';
 import { Kaptan } from '../../build/kaptan';
-import { Network, Address, RemoteService } from '../../build/network';
+import { Network, Address, RemoteService, IRemoteServiceOpts } from '../../build/network';
 
-describe('Network/RemoteService', function () {
+describe('Network/RemoteService', async function () {
   const serverKaptan = new Kaptan('server');
   const clientKaptan = new Kaptan('client');
 
-  let SERVER_PORT = 0;
-  let CLIENT_PORT = 0;
+  const SERVER_PORT = await getPort();
+  const CLIENT_PORT = await getPort();
+  
+  serverKaptan.use(Network, { PORT: SERVER_PORT });
+  clientKaptan.use(Network, { PORT: CLIENT_PORT });
 
-  before(function (done) {
-    getPort().then((sPort: number) => {
-      SERVER_PORT = sPort;
-      serverKaptan.use(Network, { PORT: SERVER_PORT });
+  let established: boolean, clientService: MyService, serverService: MyService;
 
-      getPort().then((cPort: number) => {
-        CLIENT_PORT = cPort;
-        clientKaptan.use(Network, { PORT: CLIENT_PORT });
-        done();
+  class MyService extends RemoteService {
+    constructor(kaptan: Kaptan, opts: IRemoteServiceOpts) {
+      super(kaptan, {
+        ...opts,
+        EXPOSED: {
+          SERVER: ['greet']
+        }
       });
-    });
-  });
+    }
 
-  class MyService extends RemoteService {}
+    async greet(name: string) {
+      return `Greetings, ${name}.`;
+    }
+
+    async foo() {
+      return 'foo bar';
+    }
+  }
 
   it('should create remote service as server', function () {
     serverKaptan.use(MyService);
     serverKaptan.start();
+    serverService = serverKaptan.services.spawn('MyService') as MyService;
   });
 
-  it('should throw name is required error', function (done) {
-    try {
-      new MyService(clientKaptan, { REMOTE: Address.loopback });
-      done('it did not throw error');
-    } catch (err) {
-      done();
-    }
-  });
-
-  let established = false;
   it('should create remote service as client', function () {
     clientKaptan.use(MyService, {
-      REMOTE: new Address('127.0.0.1', SERVER_PORT),
-      NAME: 'MyService'
+      REMOTE: new Address('127.0.0.1', SERVER_PORT)
     });
 
-    (<MyService>clientKaptan.services.spawn('MyService'))
-      .once('init', () => { established = true; });
-  
     clientKaptan.start();
+
+    clientService = clientKaptan.services.spawn('MyService') as MyService;
+    clientService.once('init', () => established = true);
   });
 
   it('should establish connection between services', function (done) {
     this.timeout(3000);
-    if (established) {
-      done();
-    } else {
-      (<MyService>clientKaptan.services.spawn('MyService'))
-        .once('init', () => done());
-    }
+    const check = () => established ? done() : setTimeout(check, 500);
+    check();
   });
 
   it('should update state in server and client', function (done) {
-    (<MyService>clientKaptan.services.spawn('MyService'))
+    clientService
       .on('update:num', (state) => {
         assert.equal(state.num, 5);
       })
       .on('update', () => done());
 
-    (<MyService>serverKaptan.services.spawn('MyService'))
-        .setState({ num: 5 });
+    serverService.setState({ num: 5 });
+  });
+
+  it('should run remote method on server', async function () {
+    assert.equal(
+      await clientService.server!.run('greet', ['John Doe']),
+      await serverService.greet('John Doe')
+    );
+  });
+
+  it('should not run not exposed method', async function () {
+    try {
+      const returned = await clientService.server!.run('foo', []);
+
+      if (returned === await serverService.foo()) {
+        throw new Error('exposed method run successfuly');
+      } else {
+        throw new Error('didn\'t throw error but return values don\'t match');
+      }
+    } catch (err) {
+    }
   });
 });
